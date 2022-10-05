ad03699db79a9887ee15fe9a2239fa87a90f05db
Ko <ko.webcreator@gmail.com>
2022-10-05 22:55:16 +0900

    canvasから色と位置を抽出

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,95 @@
-import { PageHead } from '../components/common/PageHead';
+import { useEffect } from 'react';
+
+import { PageHead } from '@/components/common/PageHead';
+import * as styles from '@/styles/index';
 
 import type { NextPage } from 'next';
 
+type PositionColor = {
+  blue: number;
+  green: number;
+  red: number;
+  x: number;
+  y: number;
+};
+
 const Home: NextPage = () => {
+  useEffect(() => {
+    const draftCanvas = document.getElementById('draft')! as HTMLCanvasElement;
+    const canvas = document.getElementById('header')! as HTMLCanvasElement;
+
+    draftCanvas.width = window.innerWidth;
+    draftCanvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    if (draftCanvas.getContext && canvas.getContext) {
+      const draftCtx = draftCanvas.getContext('2d') as CanvasRenderingContext2D;
+      const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+      const img = new Image(); // 新たな img 要素を作成
+      img.src = 'me.png';
+
+      img.addEventListener(
+        'load',
+        function () {
+          /*
+            image配置
+          **/
+          const imageWidth = 700;
+          const imageHeight = imageWidth * 1.11; // 比率
+          const imageX = window.innerWidth - imageWidth;
+          const imageY = window.innerHeight - imageHeight;
+          draftCtx.drawImage(img, imageX, imageY, imageWidth, imageHeight);
+
+          const imageData = draftCtx.getImageData(0, 0, canvas.width, canvas.height);
+          ctx.putImageData(imageData, 0, 0); // draft からコピーしたイメージデータを描画する ctx に持ってくる
+
+          // 参考 https://stackoverflow.com/questions/13660723/get-x-and-y-pixel-coordinates-when-iterating-over-html5-canvas-getimagedata
+          // ctx から 色と位置を保存する
+          const positionColors: PositionColor[] = [];
+          const data = imageData.data;
+          for (var i = data.length; i >= 0; i -= 4) {
+            if (data[i + 3] > 0) {
+              const red = imageData.data[i];
+              const green = imageData.data[i + 1];
+              const blue = imageData.data[i + 2];
+              if (red === 0 && green === 0 && blue === 0) return; //何も色がない所は表示しない
+              const rgba = `rgba(${red}, ${green}, ${blue})`;
+              console.log(rgba);
+              var x = (i / 4) % imageData.width;
+              var y = Math.floor(i / 4 / imageData.width);
+              const xy = `x: ${x},y: ${y}`;
+              console.log(xy);
+
+              positionColors.push({
+                blue,
+                green,
+                red,
+                x,
+                y,
+              });
+            }
+          }
+        },
+        false,
+      );
+    }
+  }, []);
+
   return (
     <>
       <PageHead description="Ko Portfolio" />
+
+      <h2 className={styles.leftFixed}>Portfolio</h2>
+      <header className={styles.header}>
+        <h1>
+          Ko
+          <br />
+          Portfolio
+        </h1>
+        <canvas className="draft" id="draft"></canvas>
+        <canvas id="header"></canvas>
+      </header>
     </>
   );
 };
---
+++ src/styles/index.ts
@@ -0,0 +1,41 @@
+import { css } from '@emotion/css';
+
+const zIndex = 20;
+
+export const leftFixed = css`
+  position: fixed;
+  left: 0;
+  top: 0;
+  font-size: 20rem;
+  z-index: 0;
+  color: var(--white);
+  writing-mode: vertical-rl;
+  opacity: 0.5;
+  text-indent: -40px;
+`;
+
+export const header = css`
+  display: flex;
+  align-items: center;
+  z-index: ${zIndex}
+  width: 100%;
+  height: 100%;
+
+  canvas {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 0;
+    left: 0;
+    top: 0;
+
+    &.draft {
+      z-index: -1;
+      opacity: 0;
+    }
+  }
+
+  h1 {
+    font-size: 10rem;
+  }
+`;
