a4a8f8180ea6d5ae8c8af6c5ce6102516b9f2dec
Ko <ko.webcreator@gmail.com>
2022-10-06 20:50:15 +0900

    ImageDataを描画する所まで確認

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import * as styles from '@/styles/index';
 import type { NextPage } from 'next';
 
 type PositionColor = {
+  alpha: number;
   blue: number;
   green: number;
   red: number;
@@ -42,32 +43,41 @@ const Home: NextPage = () => {
           draftCtx.drawImage(img, imageX, imageY, imageWidth, imageHeight);
 
           const imageData = draftCtx.getImageData(0, 0, canvas.width, canvas.height);
-          ctx.putImageData(imageData, 0, 0); // draft からコピーしたイメージデータを描画する ctx に持ってくる
 
-          // 参考 https://stackoverflow.com/questions/13660723/get-x-and-y-pixel-coordinates-when-iterating-over-html5-canvas-getimagedata
-          // ctx から 色と位置を保存する
+          /*
+            参考
+            https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Pixel_manipulation_with_canvas
+            https://stackoverflow.com/questions/13660723/get-x-and-y-pixel-coordinates-when-iterating-over-html5-canvas-getimagedata
+            ctx から 色と位置を保存する
+            **/
           const positionColors: PositionColor[] = [];
           const data = imageData.data;
+
           for (var i = data.length; i >= 0; i -= 4) {
             if (data[i + 3] > 0) {
               const red = imageData.data[i];
               const green = imageData.data[i + 1];
               const blue = imageData.data[i + 2];
-              if (red === 0 && green === 0 && blue === 0) return; //何も色がない所は表示しない
-              const rgba = `rgba(${red}, ${green}, ${blue})`;
-              console.log(rgba);
-              var x = (i / 4) % imageData.width;
-              var y = Math.floor(i / 4 / imageData.width);
-              const xy = `x: ${x},y: ${y}`;
-              console.log(xy);
+              const alpha = imageData.data[i + 3] / 255;
+
+              if (red === 0 && green === 0 && blue === 0) continue; //何も色がない所は表示しない
+              const rgba = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+
+              const x = (i / 4) % imageData.width;
+              const y = Math.floor(i / 4 / imageData.width);
 
               positionColors.push({
+                alpha,
                 blue,
                 green,
                 red,
                 x,
                 y,
               });
+
+              // 本当に再現出来るか描画してみる
+              ctx.fillStyle = `rgba(0 , 0, 0, ${alpha})`;
+              ctx.fillRect(x, y, 1, 1);
             }
           }
         },
