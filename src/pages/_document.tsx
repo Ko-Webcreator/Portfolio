22089597ba2215733e749d371fab0e4ea3115de3
Ko <ko.webcreator@gmail.com>
2023-01-01 14:15:51 +0900

    favicon や OGP 設定、画像も一部圧縮

---
+++ src/pages/_document.tsx
@@ -0,0 +1,35 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta content="福岡に住む Ko Portfolio Website です" name="description" />
+
+          {/* Favicons */}
+          <link href="/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+          <link href="/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+          <link href="/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+          <link href="/favicons/site.webmanifest" rel="manifest" />
+          <link color="#5bbad5" href="/favicons/safari-pinned-tab.svg" rel="mask-icon" />
+          <meta content="#da532c" name="msapplication-TileColor" />
+          <meta content="#ffffff" name="theme-color" />
+
+          {/* OGP */}
+          <meta content="/" property="og:url" />
+          <meta content="website" property="og:type" />
+          <meta content="/ogp.png" property="og:image" />
+          <meta content="Ko Portfolio" property="og:title" />
+          <meta content="福岡に住む Ko Portfolio Website です" property="og:description" />
+          <meta content="summary_large_image" name="twitter:card" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 import Image from 'next/image';
 import { useCallback, useEffect, useRef } from 'react';
 import Particles from 'react-particles';
@@ -61,148 +62,157 @@ const Home: NextPage = () => {
   }, []);
 
   return (
-    <div className={`${WholeStyles.wrap}`}>
-      <div className={WholeStyles.childWrap}>
-        <PageHead description="Ko Portfolio" />
-        <header className={HeaderStyles.title} ref={header}>
-          <AnimateCanvas />
-          <h1>
-            Ko
-            <br />
-            Portfolio
-          </h1>
-        </header>
-        <div className={BlocksStyles.wrap} ref={blocks}>
-          <div className={BlocksStyles.arrow}>
-            <div className={BlocksStyles.arrowInner} />
+    <>
+      <Head>
+        <title>Ko Portfolio</title>
+      </Head>
+      <div className={`${WholeStyles.wrap}`}>
+        <div className={WholeStyles.childWrap}>
+          <PageHead description="Ko Portfolio" />
+          <header className={HeaderStyles.title} ref={header}>
+            <AnimateCanvas />
+            <h1>
+              Ko
+              <br />
+              Portfolio
+            </h1>
+          </header>
+          <div className={BlocksStyles.wrap} ref={blocks}>
+            <div className={BlocksStyles.arrow}>
+              <div className={BlocksStyles.arrowInner} />
+            </div>
           </div>
-        </div>
-        <section className={RectStyles.wrap} ref={section}>
-          <h2 className={RectStyles.leftFixed} data-text="Portfolio">
-            Portfolio
-          </h2>
-          <Particles
-            className={BlocksStyles.particles}
-            id="tsparticles"
-            init={particlesInit}
-            options={config}
-          />
-          <div className={RectStyles.fukuoka}>
-            <Image alt="" layout="fill" src="/fukuoka.png" />
-          </div>
-          <div className={RectStyles.rect} ref={rect} />
-          <p className={RectStyles.copyright} data-text="2023 Ko Portfolio">
-            &copy; 2023 Ko Portfolio
-          </p>
-        </section>
-        <div className={BlocksStyles.secondBlocks} ref={secondBlock} />
-        <main className={MainStyles.wrap} ref={main}>
-          <section className={MainStyles.section} ref={firstSectionRef}>
-            <figure>
-              <a href="https://mercan.mercari.com/" rel="noreferrer" target="_blank" />
-              <iframe src="https://mercan.mercari.com/" />
+          <section className={RectStyles.wrap} ref={section}>
+            <h2 className={RectStyles.leftFixed} data-text="Portfolio">
+              Portfolio
+            </h2>
+            <Particles
+              className={BlocksStyles.particles}
+              id="tsparticles"
+              init={particlesInit}
+              options={config}
+            />
+            <div className={RectStyles.fukuoka}>
+              <Image alt="" layout="fill" src="/fukuoka.png" />
+            </div>
+            <div className={RectStyles.rect} ref={rect} />
+            <p className={RectStyles.copyright} data-text="2023 Ko Portfolio">
+              &copy; 2023 Ko Portfolio
+            </p>
+          </section>
+          <div className={BlocksStyles.secondBlocks} ref={secondBlock} />
+          <main className={MainStyles.wrap} ref={main}>
+            <section className={MainStyles.section} ref={firstSectionRef}>
+              <figure>
+                <a href="https://mercan.mercari.com/" rel="noreferrer" target="_blank" />
+                <iframe src="https://mercan.mercari.com/" />
 
-              <Image alt="" layout="fill" src="/mac.png" />
-            </figure>
-            <article id="first_article">
-              <h2>オウンドメディア</h2>
-              <p>
-                <b>使用技術 :</b>
-                <span data-slot="NuxtJS, WordPress, GraphQL" />
-              </p>
-              <p>
-                <b>製作期間 / 人数 :</b>
-                <span data-slot="約4ヶ月 / 1人" />
-              </p>
-              <p>
-                <b>説明 :</b>
-                <span
-                  data-slot="WordPress を Headless にして GraphQL にて API
+                <Image alt="" layout="fill" src="/mac.png" />
+              </figure>
+              <article id="first_article">
+                <h2>オウンドメディア</h2>
+                <p>
+                  <b>使用技術 :</b>
+                  <span data-slot="NuxtJS, WordPress, GraphQL" />
+                </p>
+                <p>
+                  <b>製作期間 / 人数 :</b>
+                  <span data-slot="約4ヶ月 / 1人" />
+                </p>
+                <p>
+                  <b>説明 :</b>
+                  <span
+                    data-slot="WordPress を Headless にして GraphQL にて API
                 を作成しました、ページ生成は NuxtJS の static generate 機能を使用して
                 表示しています。"
-                />
-              </p>
-            </article>
-          </section>
-          <section className={MainStyles.section} ref={secondSectionRef}>
-            <figure>
-              <a href="https://yomcoma.com/user/" rel="noreferrer" target="_blank" />
-              <iframe src="https://yomcoma.com/user/" />
+                  />
+                </p>
+              </article>
+            </section>
+            <section className={MainStyles.section} ref={secondSectionRef}>
+              <figure>
+                <a href="https://yomcoma.com/user/" rel="noreferrer" target="_blank" />
+                <iframe src="https://yomcoma.com/user/" />
 
-              <Image alt="" layout="fill" src="/mac.png" />
-            </figure>
-            <article id="second_article">
-              <h2>YOMcoma アプリ開発</h2>
-              <p>
-                <b>使用技術 :</b>
-                <span data-slot="Flutter, Firebase(認証周り)" />
-              </p>
-              <p>
-                <b>開発期間 / 人数 :</b>
-                <span data-slot="約8ヶ月 / 3人" />
-              </p>
-              <p>
-                <b>説明 :</b>
-                <span data-slot="プライベートで Flutter での開発を行ってた所、アプリ開発にアサインされました。フロントの設計から実装までを担当しました。" />
-              </p>
-            </article>
-          </section>
-          <section className={MainStyles.section} ref={thirdSectionRef}>
-            <figure>
-              <a href="https://yomcoma.com/writer/" rel="noreferrer" target="_blank" />
-              <iframe src="https://yomcoma.com/writer/" />
+                <Image alt="" layout="fill" src="/mac.png" />
+              </figure>
+              <article id="second_article">
+                <h2>YOMcoma アプリ開発</h2>
+                <p>
+                  <b>使用技術 :</b>
+                  <span data-slot="Flutter, Firebase(認証周り)" />
+                </p>
+                <p>
+                  <b>開発期間 / 人数 :</b>
+                  <span data-slot="約8ヶ月 / 3人" />
+                </p>
+                <p>
+                  <b>説明 :</b>
+                  <span data-slot="プライベートで Flutter での開発を行ってた所、アプリ開発にアサインされました。フロントの設計から実装までを担当しました。" />
+                </p>
+              </article>
+            </section>
+            <section className={MainStyles.section} ref={thirdSectionRef}>
+              <figure>
+                <a href="https://yomcoma.com/writer/" rel="noreferrer" target="_blank" />
+                <iframe src="https://yomcoma.com/writer/" />
 
-              <Image alt="" layout="fill" src="/mac.png" />
-            </figure>
-            <article id="third_article">
-              <h2>YOMcoma Web開発</h2>
-              <p>
-                <b>使用技術 :</b>
-                <span data-slot="NextJS, Firebase(認証周り), NestJS, MySQL, GCP" />
-              </p>
-              <p>
-                <b>開発期間 / 人数 :</b>
-                <span data-slot="約8ヶ月 / 3人" />
-              </p>
-              <p>
-                <b>説明 :</b>
-                <span data-slot="アプリ開発後は管理画面の開発を任さられました。フロントは NextJS を使用して開発してます。またバックエンドの実装も一部任さられました。バックエンドは NestJS と MySQL でインフラ周りは GCP を使用しています。" />
-              </p>
-            </article>
-          </section>
-          <section className={MainStyles.profile} ref={fourSectionRef}>
-            <div className={MainStyles.icon}>
-              <Image alt="" layout="fill" objectFit="cover" src="/pic.png" />
-            </div>
-            <div className={MainStyles.pin} />
-            <article>
-              <p>
-                ポートフォリオを見ていただきありがとうございます。
-                <br />
-                趣味で行ってたWeb制作を仕事で行いたく20代後半に上京しコーダーとして約6年間従事してきました。
-                <br />
-                そしてFlutterを使用したアプリ開発を趣味で行ってた所、今いる会社にて制作からアプリ開発にアサインされました。
-                <br />
-                そのあとこちらも独学で学んでた React を使用した開発にもアサインされ
-                Web開発も行いました。
-                <br />
-                また直近ですと NestJS を使用したバックエンド実装にも携わりました。
-                <br />
-                そうした中で今回地元に帰省するに辺り転職活動を行うことになりました。
-                <br />
-                今まで学んできた事をアウトプットしつつ、今後はより開発を追求したいと思ってます。
-                <br />
-                ご検討よろしくお願いします。
-              </p>
-              <a href="https://github.com/Ko-Webcreator/Portfolio" rel="noreferrer" target="_blank">
-                <span>Source code is here &#10071;&#65039;</span>
-                <span>Thanks for watching My portfolio &#10083;&#65039;</span>
-              </a>
-            </article>
-          </section>
-        </main>
+                <Image alt="" layout="fill" src="/mac.png" />
+              </figure>
+              <article id="third_article">
+                <h2>YOMcoma Web開発</h2>
+                <p>
+                  <b>使用技術 :</b>
+                  <span data-slot="NextJS, Firebase(認証周り), NestJS, MySQL, GCP" />
+                </p>
+                <p>
+                  <b>開発期間 / 人数 :</b>
+                  <span data-slot="約8ヶ月 / 3人" />
+                </p>
+                <p>
+                  <b>説明 :</b>
+                  <span data-slot="アプリ開発後は管理画面の開発を任さられました。フロントは NextJS を使用して開発してます。またバックエンドの実装も一部任さられました。バックエンドは NestJS と MySQL でインフラ周りは GCP を使用しています。" />
+                </p>
+              </article>
+            </section>
+            <section className={MainStyles.profile} ref={fourSectionRef}>
+              <div className={MainStyles.icon}>
+                <Image alt="" layout="fill" objectFit="cover" src="/pic.png" />
+              </div>
+              <div className={MainStyles.pin} />
+              <article>
+                <p>
+                  ポートフォリオを見ていただきありがとうございます。
+                  <br />
+                  趣味で行ってたWeb制作を仕事で行いたく20代後半に上京しコーダーとして約6年間従事してきました。
+                  <br />
+                  そしてFlutterを使用したアプリ開発を趣味で行ってた所、今いる会社にて制作からアプリ開発にアサインされました。
+                  <br />
+                  そのあとこちらも独学で学んでた React を使用した開発にもアサインされ
+                  Web開発も行いました。
+                  <br />
+                  また直近ですと NestJS を使用したバックエンド実装にも携わりました。
+                  <br />
+                  そうした中で今回地元に帰省するに辺り転職活動を行うことになりました。
+                  <br />
+                  今まで学んできた事をアウトプットしつつ、今後はより開発を追求したいと思ってます。
+                  <br />
+                  ご検討よろしくお願いします。
+                </p>
+                <a
+                  href="https://github.com/Ko-Webcreator/Portfolio"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <span>Source code is here &#10071;&#65039;</span>
+                  <span>Thanks for watching My portfolio &#10083;&#65039;</span>
+                </a>
+              </article>
+            </section>
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
