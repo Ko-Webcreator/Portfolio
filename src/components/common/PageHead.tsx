2dc9404e2451f2a02965facdfe1cb870b78cda17
Ko <ko.webcreator@gmail.com>
2023-01-02 12:53:29 +0900

    title修正

--- src/components/common/PageHead.tsx
+++ src/components/common/PageHead.tsx
@@ -6,7 +6,10 @@ type Props = {
 };
 
 export const PageHead = (props: Props) => {
-  const title = props.title != null ? `${props.title} | Ko Portfolio` : 'Ko Portfolio';
+  const title =
+    props.title != null
+      ? `${props.title} | Web creator Ko Portfolio Site`
+      : 'Web creator Ko Portfolio site';
   const regex = /(<([^>]+)>)/g;
   let description = props.description.replace(regex, '');
   description = description.length > 110 ? description.substring(0, 110) : description;
